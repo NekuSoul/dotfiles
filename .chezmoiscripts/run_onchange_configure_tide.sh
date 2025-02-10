428c82ad7277ea123ec777821059fcccb6edf94b
NekuSoul <git@nekusoul.de>
2025-02-10 20:16:02 +0100

    Add initial tide configuration script

---
+++ .chezmoiscripts/run_onchange_configure_tide.sh
@@ -0,0 +1,2 @@
+#! /bin/fish
+tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Solid --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=No
